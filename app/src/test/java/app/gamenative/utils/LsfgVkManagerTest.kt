3b6f4354dd3ba1543582f20a3d273dcd84ff25b4
Ethan Fontenot <e_fontenot1@yahoo.com>
2026-08-28 19:54:58 -0500

    test: cover LSFG implicit layer discovery path

---
+++ app/src/test/java/app/gamenative/utils/LsfgVkManagerTest.kt
@@ -0,0 +1,58 @@
+package app.gamenative.utils
+
+import com.winlator.container.Container
+import com.winlator.core.envvars.EnvVars
+import java.io.File
+import java.nio.file.Files
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class LsfgVkManagerTest {
+    private lateinit var rootDir: File
+
+    @Before
+    fun setUp() {
+        rootDir = Files.createTempDirectory("gamenative-lsfg-test").toFile()
+    }
+
+    @After
+    fun tearDown() {
+        rootDir.deleteRecursively()
+    }
+
+    @Test
+    fun applyLaunchEnv_usesImplicitLayerSearchPathWithoutMutatingExplicitLayerPath() {
+        File(rootDir, ".local/share/lsfg-vk/Lossless.dll").apply {
+            parentFile?.mkdirs()
+            writeBytes(byteArrayOf(1))
+        }
+
+        val container = Container("lsfg-test").apply {
+            setRootDir(rootDir)
+            setContainerVariant(Container.BIONIC)
+            putExtra(LsfgVkManager.EXTRA_ARMED, true)
+        }
+        val envVars = EnvVars().apply {
+            put("VK_LAYER_PATH", "/existing/explicit")
+            put("VK_IMPLICIT_LAYER_PATH", "/existing/implicit")
+        }
+
+        assertTrue(LsfgVkManager.applyLaunchEnv(container, envVars))
+
+        val expectedLayerDir = File(
+            rootDir,
+            ".local/share/vulkan/implicit_layer.d",
+        ).absolutePath
+        assertEquals(
+            "/existing/implicit:$expectedLayerDir",
+            envVars["VK_IMPLICIT_LAYER_PATH"],
+        )
+        assertEquals("/existing/explicit", envVars["VK_LAYER_PATH"])
+    }
+}

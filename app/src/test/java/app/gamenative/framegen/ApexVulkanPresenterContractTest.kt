736a81d346532c040179a48c58a0edf9b61a5657
Ragnarok93 <e_fontenot1@yahoo.com>
2026-09-25 22:29:04 -0500

    test(apex): import batch barrier assertion

--- app/src/test/java/app/gamenative/framegen/ApexVulkanPresenterContractTest.kt
+++ app/src/test/java/app/gamenative/framegen/ApexVulkanPresenterContractTest.kt
@@ -1,6 +1,7 @@
 package app.gamenative.framegen
 
 import java.io.File
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
